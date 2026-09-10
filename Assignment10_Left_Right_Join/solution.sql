8f5c2fcf0219f92ff0e77307512bf77b6db0c3ab
1U25CA012- APSAN MARLIYA <apsaa0960@gmail.com>
2026-09-10 19:46:55 +0530

    Update solution.sql

--- Assignment10_Left_Right_Join/solution.sql
+++ Assignment10_Left_Right_Join/solution.sql
@@ -1,13 +1,35 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
--- Create Course
-
--- Create Enrollment
-
--- Insert sample records
-
--- LEFT JOIN
-
--- RIGHT JOIN
+Create database apsan;
+use apsan;
+CREATE TABLE Course (
+    CourseID NUMBER(5),
+    CourseName VARCHAR2(30),
+    Credits NUMBER(2)
+);
+INSERT INTO Course VALUES (201, 'Database Systems', 4);
+INSERT INTO Course VALUES (202, 'Data Structures', 3);
+INSERT INTO Course VALUES (203, 'Mathematics', 4);
+CREATE TABLE Enrollment (
+    EnrollmentID NUMBER(5),
+    StudentID NUMBER(5),
+    CourseID NUMBER(5)
+);
+INSERT INTO Enrollment VALUES (1, 1001, 201);
+INSERT INTO Enrollment VALUES (2, 1001, 202);
+INSERT INTO Enrollment VALUES (3, 1002, 203);
+INSERT INTO Enrollment VALUES (4, 1003, 201);
+SELECT Course.CourseID,
+       Course.CourseName,
+       Course.Credits,
+       Enrollment.EnrollmentID,
+       Enrollment.StudentID
+FROM Course
+LEFT JOIN Enrollment
+ON Course.CourseID = Enrollment.CourseID;
+SELECT Course.CourseID,
+       Course.CourseName,
+       Course.Credits,
+       Enrollment.EnrollmentID,
+       Enrollment.StudentID
+FROM Course
+RIGHT JOIN Enrollment
+ON Course.CourseID = Enrollment.CourseID;
